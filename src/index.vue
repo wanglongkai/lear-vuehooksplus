622972856c054c37d219bffb21c75b451cc03fb9
wanglongkai1995 <2064819994@qq.com>
2025-04-08 14:05:20 +0800

    project: prettier配置

--- src/index.vue
+++ src/index.vue
@@ -1,9 +1,7 @@
 <template>
-    <UseToggle/>
+  <UseToggle />
 </template>
 <script lang="ts" setup>
-    import UseToggle from './test/_test_useToggle.vue'
+  import UseToggle from './test/_test_useToggle.vue'
 </script>
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
